a5ba995e3867e5429a79ebf5625463ff6c8acb13
bawumbila <74751215+bawumbila@users.noreply.github.com>
2026-02-04 18:39:37 -0800

    allow multiple origins

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 // require our modules
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 //TODO: require cors module
-const cors = require('cors');
+const cors = require("cors");
+//Allow multiple origins
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-frontend-app.herokuapp.com",
+];
 // initiate the express app
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 // configure settings app.set()
 //TODO: require and configure our dotenv module
-require('./config/database');
+require("./config/database");
 //TODO: require our database config file
 // mount middleware with app.use()
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json()); //converts incoming json into req.body
 app.use(cors());
 // mount our route with app.use()
-app.use('/api/tracks', require('./routes/api/tracks'));
+app.use("/api/tracks", require("./routes/api/tracks"));
 // tell the app listen to port 3001
 const port = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }),
+);
 
 app.listen(port, function () {
   console.log(`Express is listening for AJAX request on port ${port}`);
