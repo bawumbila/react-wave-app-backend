c66d4059a11bd110842d480cd3fe2861ec081c4b
bawumbila <74751215+bawumbila@users.noreply.github.com>
2026-02-04 23:57:05 -0800

    fixed allow origins

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const morgan = require("morgan");
 //TODO: require cors module
 const cors = require("cors");
+// initiate the express app
+const app = express();
 //Allow multiple origins
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://react-wave-app-frontend2.herokuapp.com",
+  "https://react-wave-app-frontend2.netlify.app",
 ];
-// initiate the express app
-const app = express();
 require("dotenv").config();
 // configure settings app.set()
 //TODO: require and configure our dotenv module
@@ -27,14 +27,19 @@ const port = process.env.PORT || 3001;
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("Not allowed by CORS"), false);
       }
+      return callback(null, true);
     },
+    credentials: true,
   }),
 );
+
+app.use(express.json());
 
 app.listen(port, function () {
   console.log(`Express is listening for AJAX request on port ${port}`);
